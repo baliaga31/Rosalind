45a6c0b753a34e8468799be2a437ea758a830a9f
wanaga3166 <benoit-aliaga@orange.fr>
2019-10-27 11:55:51 +0100

    ajout raise pour len(S1)==len(S2)

--- Bioinformatics_Stronghold/04_Counting_Point_Mutations/Counting_Hamming_Distance.py
+++ Bioinformatics_Stronghold/04_Counting_Point_Mutations/Counting_Hamming_Distance.py
@@ -7,11 +7,9 @@ if len(S1) == len(S2):
 
   Hamming_Distance = 0
 
-# hamming_distance = [i for i in range(len(strand_a)) if strand_a[i] != strand_b[i]]
-
   for n in range(len(S1)):
     if S1[n] != S2[n]:
       Hamming_Distance += 1
   print(Hamming_Distance)
 else:
-  print("One sequence is too short or too long.")
+  raise Exception("One sequence is too short or too long.")
