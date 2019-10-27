e76c88496a76e179b5eb92b68f2d386e690efa81
wanaga3166 <benoit-aliaga@orange.fr>
2019-10-27 12:40:01 +0100

    Ajout Docstring + informations complementaires

--- Bioinformatics_Stronghold/05_Computing_GC_Content/Computing_GC.py
+++ Bioinformatics_Stronghold/05_Computing_GC_Content/Computing_GC.py
@@ -2,5 +2,21 @@
 
 """
 Script which count the %GC. 
+
+This script will read fasta sequences and compute the GC percentage
 """
 
+__authors__ = ("Benoit Aliaga")
+__contact__ = ("benoit.aliaga@umontpellier.fr")
+__version__ = "0.0.1"
+__date__ = "10/27/2019"
+
+def Fasta():
+    pass
+
+def ComputeGC():
+    pass
+
+if __name__ == "__main__":
+    # The main programm start here
+    pass 
