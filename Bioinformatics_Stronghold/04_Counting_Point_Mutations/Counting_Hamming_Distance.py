2835906f6387c6fdf5c31094f17ed23d44e6ed24
wanaga3166 <benoit-aliaga@orange.fr>
2019-10-27 02:09:44 +0100

    Premiere version d'un script qui donne la bonne reponse. A ameliorer
    quita

--- Bioinformatics_Stronghold/04_Counting_Point_Mutations/Counting_Hamming_Distance.py
+++ Bioinformatics_Stronghold/04_Counting_Point_Mutations/Counting_Hamming_Distance.py
@@ -4,6 +4,14 @@ S1="GAGCCTACTAACGGGAT"
 S2="CATCGTAATGACGGCCT"
 
 if len(S1) == len(S2):
-  print("It's OK!")
+
+  Hamming_Distance = 0
+
+# hamming_distance = [i for i in range(len(strand_a)) if strand_a[i] != strand_b[i]]
+
+  for n in range(len(S1)):
+    if S1[n] != S2[n]:
+      Hamming_Distance += 1
+      print(Hamming_Distance)
 else:
   print("One sequence is too short or too long.")
